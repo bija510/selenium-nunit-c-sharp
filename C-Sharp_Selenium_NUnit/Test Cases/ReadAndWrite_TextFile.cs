8caf9a5e9e34c41eb08d97aaf5ea74df85398640
bija510 <error.zxz@gmail.com>
2020-08-15 22:33:14 -0400

    Added Read&Write .txt file

---
+++ C-Sharp_Selenium_NUnit/Test Cases/ReadAndWrite_TextFile.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace C_Sharp_Selenium_NUnit.Test_Cases
+{
+    [TestFixture]
+    class ReadAndWrite_TextFile
+    {
+        [Test]
+        public static void readwrite()
+        {          
+            try
+            {
+                string filePath = @"C:\Users\Bijaya Chhetri\Documents\Selenium-C#\C-Sharp_Selenium_NUnit\C-Sharp_Selenium_NUnit\Data\TextFile.txt";          
+                List<string> lines = new List<string>();
+                lines = File.ReadLines(filePath).ToList();
+
+                foreach (String line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                lines.Add("Hello world");
+                File.WriteAllLines(filePath, lines);              
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
+
+
+
+
