271b3c2f3858dcce057853155247f0ec00bd933b
bija510 <error.zxz@gmail.com>
2020-08-29 23:28:03 -0400

    Added c#-full_course & headlessBrowser Execution

---
+++ C-Sharp_Selenium_NUnit/C-Sharp_FullCourse/all_basic.cs
@@ -0,0 +1,179 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Selenium_NUnit.C_Sharp_FullCourse
+{
+    [TestFixture]
+    class all_basic
+    {
+        int num, num2;
+        double dub;
+        string str, str1;
+        bool b;
+        char c;
+
+        [Test]
+        public void dataType() 
+            {
+                /*************************************
+                 * DataTypes
+                 *************************************/
+                num = 5;
+                num2 = 10;
+                dub = 1.4;
+                str = "ball";
+                str1 = "apple";
+                b = true;
+                c = 'd';
+
+                Console.WriteLine(num + num2);
+                Console.WriteLine(str.ToUpper());
+                Console.WriteLine(str[0]);
+            }
+            /*************************************
+             * Condational statement if and Else
+             *************************************/
+        [Test]
+        public void CondationalIfAndElse()
+        {
+            if (num < num2)
+            {
+                Console.WriteLine("true one");
+            }
+            else if (str == str1)
+            {
+                Console.WriteLine("true two");
+            }
+            else
+            {
+                Console.WriteLine("none of the statement is true");
+            }
+        }
+
+        [Test]
+        public void forLoop() {
+            /*************************************
+             * for loop
+             *************************************/
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("=====================");
+        }
+
+        [Test]
+        public void whileLoop() {
+            /*************************************
+           * While loop
+           *************************************/
+            int j = 0;
+            while (j <= 5)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+        }
+
+        [Test]
+        public void arraysType() {
+            /******************************************
+            * Array one-dimensional
+            ******************************************/
+            int[] arra = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(arra[1]);
+
+            String[] strArra = { "car", "truck", "Bike" };
+            Console.WriteLine(strArra[0]);
+
+            /******************************************
+            * Array Two-dimensional
+            ******************************************/
+            int[,] arra1 = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 } };
+            Console.WriteLine(arra1[0, 0]);
+            Console.WriteLine(arra1[1, 0]);
+
+            //Math
+            Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.Round(9.99));
+        }
+        [Test]
+        public void switchCase()
+        {
+            /******************************************
+            * Switch case
+            ******************************************/
+            int x = 2;
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("case one");
+                    break;
+                case 2:
+                    Console.WriteLine("case two");
+                    break;
+                default:
+                    Console.WriteLine("None of them match");
+                    break;
+
+            }
+            //Break and continue
+
+            for (int l = 0; l < 3; l++)
+            {
+                if (l == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(l);
+            }
+        }
+        [Test]
+        public void methodCalling()
+        {
+            /******************************************
+            * Method calling
+            ******************************************/
+            Console.WriteLine(car(10, 5));
+
+            food();
+
+            //CALLING MEHTOD OVERLOADING
+            Console.WriteLine(PlusMethod(2, 3));
+            Console.WriteLine(PlusMethod(1.2, 2.2));
+            Console.ReadLine();
+        }
+    
+
+        public static void food() 
+        {
+            Console.WriteLine("MOMO");
+        }
+
+        static int car(int a, int b)
+        {
+            Console.WriteLine("bmw is my fav car");
+            return (a + b);
+        }
+
+        //METHOD OVERLOADING
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+    }
+
+}
+
+    
+
--- C-Sharp_Selenium_NUnit/Test Cases/JavaScriptFunction.cs
+++ C-Sharp_Selenium_NUnit/Test Cases/JavaScriptFunction.cs
@@ -17,24 +17,26 @@ namespace C_Sharp_Selenium_NUnit.Test_Cases
         [Test]
         public void JSFunction()
         {
+            //NOte >>-----> [s]string and [S]String are same coz they both are same type <<<<--System.String-->>> Method==> .GetType().FullName
 
             var Driver = new BrowserUtility().InitChrome(driver);
-            Driver.Url = "https://www.facebook.com/";
+            Driver.Url = "http://demo.automationtesting.in/Register.html";
             IJavaScriptExecutor js = ((IJavaScriptExecutor)Driver);
 
             //SetText Using JavaScript function
-            IWebElement firstName = Driver.FindElement(By.XPath("//input[@id='u_0_m']"));
-            js.ExecuteScript("arguments[0].value='apple';", firstName);
+            String aFor = "david";
+            IWebElement firstName = Driver.FindElement(By.XPath("//input[@placeholder='First Name']"));
+            js.ExecuteScript("arguments[0].value='"+aFor+"';", firstName);
             Thread.Sleep(2000);
 
             //GetText Using JavaScript function
-            IWebElement headerTest = Driver.FindElement(By.XPath("//h2[@class='_8est']"));
+            IWebElement headerTest = Driver.FindElement(By.XPath("//h1[contains(text(),'Automation Demo Site')]"));
             Console.WriteLine(js.ExecuteScript("return (arguments[0].innerHTML).toString();", headerTest));
 
             Thread.Sleep(2000);
 
             //Click Using JavaScript function
-            IWebElement forgetPasswordLink = Driver.FindElement(By.XPath("//a[contains(text(),'Forgot account?')]"));
+            IWebElement forgetPasswordLink = Driver.FindElement(By.XPath("//input[@id='checkbox1']"));
             js.ExecuteScript("arguments[0].click();", forgetPasswordLink);
 
             js.ExecuteScript("window.scrollBy(0, 1000)");
